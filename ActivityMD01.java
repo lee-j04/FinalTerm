346d56fdffbbaa355e416be1cff9f3fe53a9c9d9
DESKTOP-3NHFKQP\User <>
2024-10-14 10:30:46 +0800

    new act

--- ActivityMD01.java
+++ ActivityMD01.java
@@ -10,18 +10,17 @@
 public class ActivityMD01 {
     public static void main(String[] args) {
         
-        char[][] array = new char [3][4];
+        char[][] array = new char [4][];
         
-        for(int i = 0; i < array.length; i++)
-        {
-            for(int j = 0; j < array[i].length; j++) 
-            {
+        for(int i = 0; i < array.length; i++) {
+            
+            array[i] = new char[i+1]; 
+            for(int j = 0; j < array[i].length; j++) {
                array[i][j] = '*';
             }
         }
         
-        for(int i = 0; i < array[i].length; i++)
-        { 
+        for(int i = 0; i < array.length; i++){ 
             for(int j = 0; j < array[i].length; j++)
             {
                 System.out.print(array[i][j] + " ");                                         
